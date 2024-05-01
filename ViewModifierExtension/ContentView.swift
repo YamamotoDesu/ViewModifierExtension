c80460403603ffd7465b40c1fd99e632f2c76b20
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-05-01 17:13:02 +0900

    Initial Commit

---
+++ ViewModifierExtension/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewModifierExtension
+//
+//  Created by Yamamoto Kyo on 2024/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewModifierExtension/ViewModifierExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierExtensionApp.swift
+//  ViewModifierExtension
+//
+//  Created by Yamamoto Kyo on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
